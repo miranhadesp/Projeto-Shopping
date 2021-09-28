8a021e69b3479252467b42cfc230fe63cbbca52d
Diogo Torres Batista <57422936+Diogo277@users.noreply.github.com>
2021-09-28 13:00:23 -0300

    Update menu.cs

--- menu.cs
+++ menu.cs
@@ -1,53 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace projeto1{
+namespace Testess
+{
+    class Inicio{
+        public void Select(Dictionary <int, string> fastfood , Dictionary <int, string> selfservice){
+            int n;
+            Console.WriteLine("1 - FastFood");
+            Console.WriteLine("2 - selfservice");
+            Console.Write("n: ");
+            Int32.TryParse(Console.ReadLine(), out n);
 
-    public class Menu{
+            switch(n){
+                case 1:
+                    FastFood fast = new FastFood();
+                    fast.Menu1(fastfood, selfservice);
+                    break;
+                case 2:
+                    SelfService self = new SelfService();
+                    self.Menu2(selfservice, fastfood);
+                    break;
 
-         public static void Inicio(){       //-> Victor Augusto
-
-            Console.WriteLine("\n1 - Cadastrar");
-            Console.WriteLine("2 - Remover");
-            Console.WriteLine("3 - Simular compra");
-            Console.WriteLine("0 - Sair do programa");
-
-            int opcao = Convert.ToInt32(Console.ReadLine());
-
-            if(!(opcao == 1 || opcao == 2 || opcao == 3)){
-                Console.WriteLine("Opção inválida, tente novamente");
-                Inicio();
-            } 
-
-            else{
-                string op = Convert.ToString(opcao);
-
-                Segmento(op);
             }
         }
-            
-        public static void Segmento(string controle){
-
-            Console.WriteLine("\n1 - FastFood");
-            Console.WriteLine("2 - SelfService");
-            Console.WriteLine("3 - Loja");
-            Console.WriteLine("0 - Voltar ao menu");
-
-            int opcao = Convert.ToInt32(Console.ReadLine());
-
-            if(!(opcao == 1 || opcao == 2 || opcao == 3)){
-                Console.WriteLine("Opção inválida, tente novamente");
-
-                Segmento(controle);
-            } 
-
-            else{
-                string op = Convert.ToString(opcao);
-
-                string str = controle + op;
-
-                Controle.Switchs(str);
-            } 
-        } 
     }
 }
